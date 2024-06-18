413b75f80f559a1e2b1f844c85d26a49188727e5
omer861 <122231964+omer861@users.noreply.github.com>
2024-06-18 15:40:53 +0530

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -6,13 +6,13 @@ sudo yum update -y
 sudo yum install -y httpd
 
 # Start the Apache HTTP Server
-sudo systemctl start httpd
+sudo service httpd start
 
 # Enable Apache to start on boot
-sudo systemctl enable httpd
+sudo chkconfig httpd on
 
 # Create the web page
 echo "Welcome to page1" | sudo tee /var/www/html/index.html
 
 # Restart Apache to ensure all changes are applied
-sudo systemctl restart httpd
+sudo service httpd restart
